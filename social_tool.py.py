dd007ad94a85108b8d15b67681689bdbee2583d5
alimoto74 <erbil.arabic@gmail.com>
2026-04-28 13:46:00 +0300

    Update social_tool.py.py

--- social_tool.py.py
+++ social_tool.py.py
@@ -5,7 +5,7 @@ import google.generativeai as genai
 genai.configure(api_key="AIzaSyDvgGljJPtGBkZwFvCO4QHAeGKQmVsxKaw")
 
 # 2. تعريف الموديل
-model = genai.GenerativeModel('gemini-pro')
+model = genai.GenerativeModel('gemini-1.5-flash')
 
 st.set_page_config(page_title="محلل السوشيال ميديا المحمي", layout="wide")
 
